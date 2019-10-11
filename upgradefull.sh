301f0937de4ba0a2dd866b636c355bf8c1389c2f
Spencer R Hall <spencer.r.hall@protonmail.com>
2019-10-10 23:10:38 -0700

    updatescript.sh update

--- upgradefull.sh
+++ upgradefull.sh
@@ -7,8 +7,19 @@ yay -Syu
 sudo snap refresh
 sudo flatpak update
 yay -Rns $(yay -Qttdq)
+
+set -eu
+
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        sudo snap remove "$snapname" --revision="$revision"
+    done
+
 sudo paccache -ruk0
 sudo paccache -rk2
+
+sudo journalctl --vacuum-files=1
+
 read -p "Press [Enter] to close"
 exit
 echo "Full upgrade complete. Enjoy the latest packages!"
